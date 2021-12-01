686abf3a6f000a996fa47d72cd83adf3147f7f12
Pyxless <95367189+Pyxless@users.noreply.github.com>
2021-12-02 00:22:36 +0300

    Add files via upload

---
+++ constants/catalog.js
@@ -0,0 +1,15 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: '  Джинсовая куртка xxxtentacion',
+        img: 'товар2.jpg',
+        price: 17.99,
+    },
+    {
+        id: 'el2',
+        name: 'Мужская толстовка 3D с принтом Hajime',
+        img: 'товар.jfif',
+        price: 19.99,
+    },
+    
+];
